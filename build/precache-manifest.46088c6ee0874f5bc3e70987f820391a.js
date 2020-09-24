7c62c846173fab145960abd389248365394dbcca
DavidCastillo4 <David.Castillo4@gmail.com>
2020-09-23 19:59:19 -0500

    init

---
+++ build/precache-manifest.46088c6ee0874f5bc3e70987f820391a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce79830ffc293fa0b37e2ec25737ee5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "690c261b62ebba9ddbe1",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d7fcbc14c79c93687c6",
+    "url": "./static/js/2.1215340d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1215340d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690c261b62ebba9ddbe1",
+    "url": "./static/js/main.bf497e49.chunk.js"
+  },
+  {
+    "revision": "840f43a6987b0f351eda",
+    "url": "./static/js/runtime-main.f3545b84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
